1694370deac73cd89e9060e10476fedf6271f59f
Bing Du <bing.du@earthsoft.com>
2022-10-24 11:39:47 -0600

    not sure if we need these extra /Scripts/*.*, but push it just in case.

---
+++ WebApplication3/WebApplication3/Scripts/index.js
@@ -0,0 +1,47 @@
+﻿////(function () {
+////  'use strict';
+////  angular.module('app', [])
+////    .controller('DIController', DIController);
+////  DIController.$inject = ['$scope', '$filter'];
+////  function ($scope, $filter) {
+////    $scope.name = "Yak";
+////    $scope.upper = function () {
+////      Var upCase = $filter('uppercase');
+////      $scope.name = upCase($scope.name);
+////    };
+////  };
+////}) ();
+
+(function () {
+  'use strict';
+  angular.module('myApp', [])
+    .controller('DIController',
+      function () {
+        //$scope.name = "Yak";
+        //$scope.upper = function () {
+        //  Var upCase = $filter('uppercase');
+        //  $scope.name = upCase($scope.name);
+        //};
+      });
+})();
+
+//var myApp = angular.module('myApp', [])
+//    .controller('DIController',
+//      function ($scope) {
+//        $scope.name = "Yak";
+//        //$scope.upper = function () {
+//        //  Var upCase = $filter('uppercase');
+//        //  $scope.name = upCase($scope.name);
+//        //};
+//      });
+
+//function DIController($scope) {
+//  $scope.name = ["abcd@gmail.com", "abcd@yahoo.co.in"];
+
+//  $scope.add = function () {
+//    $scope.name.push($scope.newcontact);
+//    $scope.newcontact = "";
+//  };
+//}
+//DIController.$inject = ['$scope'];
+//angular.module('myApp', []).controller('DIController', DIController);
