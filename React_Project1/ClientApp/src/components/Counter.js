5b8dd6a0006c46d642dcb2dd7991f87b63803e98
Bing Du <bing.du@earthsoft.com>
2022-12-17 22:52:38 -0700

    how to create/use newMethod; how to do ng-repeat li in React; how to load local images

--- React_Project1/ClientApp/src/components/Counter.js
+++ React_Project1/ClientApp/src/components/Counter.js
@@ -6,7 +6,7 @@ export class Counter extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { currentCount: 0, formatCount: 'Zero' }  //, imageUrl: require('../images/fall_trees_scene.jpg') }; // imageUrl... works too.
+    this.state = { currentCount: 0, formatCount: 'Zero', tags:['tag1','tag2','tag3'] }  //, imageUrl: require('../images/fall_trees_scene.jpg') }; // imageUrl... works too if not use import fall_trees_scene from '../images...'
     this.incrementCounter = this.incrementCounter.bind(this);
     this.decreaseCounter = this.decreaseCounter.bind(this);
   }
@@ -25,6 +25,22 @@ export class Counter extends Component {
     });
   }
 
+  // "btn btn-primary m-2": m-2 means margin 2px which creates a bit space between Increment and Decrease button 
+  // btn-sm: means small button.
+  // badge-warning: display the button with yellow background.
+  //
+
+  // Under Line render, type 
+  //   let classes = 'btn btn-sm badge-';
+  //   classes += (this.state.currentCount === 0 ? 'warning' : 'secondary');
+  // Select above 2 lines and right-click to select Quick action and refactoring
+  // newMethod was created and then change newMethod to a meaningful name, i.e. getBadgesClasses
+  getBadgesClasses() {
+        let classes = 'btn btn-sm badge-';
+        classes += (this.state.currentCount === 0 ? 'warning' : 'secondary');
+        return classes;
+  }
+
   render() {
     return (
       <div>
@@ -34,9 +50,13 @@ export class Counter extends Component {
 
         <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
         <p>Formatted Count: {this.state.formatCount}</p>
-        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
-        <button className="btn btn-secondery" onClick={this.decreaseCounter}>Decrease</button>
+        <button className="btn btn-primary m-2" onClick={this.incrementCounter}>Increment</button>
+        <button className={this.getBadgesClasses()} onClick={this.decreaseCounter}>Decrease</button>
+        <ul>
+          {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}   {/*it equals ng-repeat. A. key={tag} to avoid 'Warning: Each child in a list should have a unique "key" prop.' B. being unique in this list is limited to this line scope */}
+        </ul>
       </div>
     );
   }
+   
 }
