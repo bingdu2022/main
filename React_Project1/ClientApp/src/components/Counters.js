0a04a72768ab28de978088b8c4f676c11cfdf86f
Bing Du <bing.du@earthsoft.com>
2022-12-19 22:21:04 -0700

    minors

--- React_Project1/ClientApp/src/components/Counters.js
+++ React_Project1/ClientApp/src/components/Counters.js
@@ -4,6 +4,11 @@ import { Counter } from './Counter';  // ./Counter tells it's at the same level 
 export class Counters extends Component {
   state = { counters: [{ id: 1, value: 4 }, { id: 2, value: 2 }] };
 
+  handleReset = () => {
+    const counters = this.state.counters.map(x => { x.value = 0; return x; });
+    this.setState({ counters });
+  }
+
   handleDeleteCounter = (x) => {   /* add x or countId to know which counter in the state.counters to remove*/
     console.log('Even Handler Called: ', x)  /*click Delete Me button to show it comes here: it should show Even Handler Called: 1*/
 
@@ -18,11 +23,12 @@ export class Counters extends Component {
       <div>
         <h2>Counters: links to 2 counters</h2>
         <h4>pass data from Counters to Counter; raise and handle events; multiple components in sync; functional components; lifecyle hooks </h4>
-
+        <button className="btn btn-primary m-2" onClick={this.handleReset}>Reset Me</button>
         {/*  <Counter />*/}
         {/*  <Counter></Counter>*/}
         {/*above = below which = ng-repeat*/}
         {this.state.counters.map(x => <Counter key={x.id} value={x.value} otherWayPassArgCounterId={x.id} onDelete={this.handleDeleteCounter}> <h4>Passing Counter #{x.id} from Counters.js:</h4> </Counter>)};   {/*Counter (cannot be counter) comes from Line 2. state.counters comes from state = { counters: ..}*/}
+        {/*the key={x.id} above is never treated as arg and all the others are arguments. value=.. otherw.. can be placed into one object like counter={x} */}
         {/*the value={x.value} above is one of props and can be passed into Counter - see Counter.js > constructor(props) { . this.props.value ..} */}
         {/*Counters.js or the <h4>Counter #{counter.id}</h4> above is chidren of the Counter's props, so it can be passed to Counter if Counter.js has {this.props.children} - see Counter.js > */}
       </div>
