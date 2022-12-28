414e06b9317eecfe248af23f8970b7314beed70b
Bing Du <bing.du@earthsoft.com>
2022-12-27 22:33:35 -0700

    Created WebAPI_Core

---
+++ WebAPI_Core/Controllers/DepartmentController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace WebAPI_Core.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class DepartmentController : Controller
+  {
+
+    private readonly IConfiguration _configuration;
+
+    public DepartmentController(IConfiguration configuration)
+    {
+      _configuration = configuration;
+    }
+
+    [HttpGet]
+    public JsonResult Get()
+    {
+      string query = @"select DepartmentId, DepartmentName from dbo.Department";
+      DataTable table = new DataTable();
+      string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+      SqlDataReader myReader;
+      using(SqlConnection myCon = new SqlConnection(sqlDataSource))
+      {
+        myCon.Open();
+        using (SqlCommand myCommand = new SqlCommand(query, myCon))
+        {
+          myReader = myCommand.ExecuteReader();
+          table.Load(myReader);
+          myReader.Close();
+          myCon.Close();
+        }
+      }
+      return new JsonResult(table);
+    }
+
+    // GET: DepartmentController
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    // GET: DepartmentController/Details/5
+    public ActionResult Details(int id)
+    {
+      return View();
+    }
+
+    // GET: DepartmentController/Create
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    // POST: DepartmentController/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(IFormCollection collection)
+    {
+      try
+      {
+        return RedirectToAction(nameof(Index));
+      }
+      catch
+      {
+        return View();
+      }
+    }
+
+    // GET: DepartmentController/Edit/5
+    public ActionResult Edit(int id)
+    {
+      return View();
+    }
+
+    // POST: DepartmentController/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(int id, IFormCollection collection)
+    {
+      try
+      {
+        return RedirectToAction(nameof(Index));
+      }
+      catch
+      {
+        return View();
+      }
+    }
+
+    // GET: DepartmentController/Delete/5
+    public ActionResult Delete(int id)
+    {
+      return View();
+    }
+
+    // POST: DepartmentController/Delete/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Delete(int id, IFormCollection collection)
+    {
+      try
+      {
+        return RedirectToAction(nameof(Index));
+      }
+      catch
+      {
+        return View();
+      }
+    }
+  }
+}
---
+++ WebAPI_Core/Startup.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
+
+
+namespace WebAPI_Core
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+      // Enable CORS: from 'VS2019-Learn React JS and .NET Core API by Creating a Full Stack Web App from Scratch.mp4'
+      services.AddCors(c => {
+        c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+      });
+      //JSON Serializer: set it as default and need Newtonsoft package to do the below
+      // Right-click on Solution, select Manage NuGet ..., search mvc.newtonsoft, select Miscrosoft.AspNetCore.Mvc.NewtonsoftJson: version 3.1.9
+      services.AddControllersWithViews()
+        .AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft
+      .Json.ReferenceLoopHandling.Ignore)
+        .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver
+        = new DefaultContractResolver());
+
+      services.AddControllers();
+
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      // Enable CORS: from 'VS2019-Learn React JS and .NET Core API by Creating a Full Stack Web App from Scratch.mp4'
+      app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
+}
