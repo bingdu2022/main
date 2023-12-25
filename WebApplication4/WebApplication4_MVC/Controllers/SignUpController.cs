56ae12f8f5d8d3151cd18b60c0eb0e85aba4ff2f
Bing Du <bingduus2023@hotmail.com>
2023-12-25 15:58:46 -0700

    adding DataLibrary and MVC_Database ... it worked ...

---
+++ WebApplication4/WebApplication4_MVC/Controllers/SignUpController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4_MVC.Models;
+
+namespace WebApplication4_MVC.Controllers
+{
+  public class SignUpController : Controller
+  {
+    // GET: SignUp
+    public ActionResult SignUp()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult SignUp(EmployeeModel model)
+    {
+      if (ModelState.IsValid)  // it's a second layer of security
+      {
+        return RedirectToAction("Index", "Home");
+      }
+      return View();
+    }
+  }
+
+}
