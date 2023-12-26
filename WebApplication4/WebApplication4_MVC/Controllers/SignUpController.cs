e9f176f609f17e24bf011b3cb27759b3dc13b562
Bing Du <bingduus2023@hotmail.com>
2023-12-26 16:23:48 -0700

    working version of employee sign up, including verifying client-side (or UI) new employee info input, saving them to database and display them from database.

--- WebApplication4/WebApplication4_MVC/Controllers/SignUpController.cs
+++ WebApplication4/WebApplication4_MVC/Controllers/SignUpController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication4_MVC.Models;
+using static DataLibrary.BusinessRules.EmployeeProcessor;
 
 namespace WebApplication4_MVC.Controllers
 {
@@ -15,13 +16,36 @@ namespace WebApplication4_MVC.Controllers
       return View();
     }
 
+    public ActionResult ViewEmployee()
+    {
+      var data = LoadEmployees();
+      List<EmployeeModel> employeeModels = new List<EmployeeModel>();
+      foreach (var row in data)
+      {
+        employeeModels.Add(new EmployeeModel
+        {
+          EmployeeId = row.EmployeeId,
+          FirstName = row.FirstName,
+          LastName = row.LastName,
+          EmailAddress = row.EmailAddress,
+          ConfirmEmail = row.EmailAddress
+        });
+      }
+
+      return View(employeeModels);
+
+    }
+
+
     [HttpPost]
     [ValidateAntiForgeryToken]
     public ActionResult SignUp(EmployeeModel model)
     {
       if (ModelState.IsValid)  // it's a second layer of security
       {
-        return RedirectToAction("Index", "Home");
+        int count = CreateEmployee(model.EmployeeId, model.FirstName, model.LastName, model.EmailAddress);
+
+        return RedirectToAction("ViewEmployee", "SignUp");
       }
       return View();
     }
