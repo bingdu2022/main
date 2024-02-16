035843f3880e06ced5f38e552d6344a91e212eb2
Bing Du <bingduus2023@hotmail.com>
2024-02-15 23:52:15 -0700

    draft 1 of SqlKeywordConverter

---
+++ SqlKeywordConverter/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace SqlKeywordConverter
+{
+  partial class MainForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.SqlCodeTextBox = new System.Windows.Forms.TextBox();
+      this.ConvertButton = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // SqlCodeTextBox
+      // 
+      this.SqlCodeTextBox.AllowDrop = true;
+      this.SqlCodeTextBox.Location = new System.Drawing.Point(2, 38);
+      this.SqlCodeTextBox.Multiline = true;
+      this.SqlCodeTextBox.Name = "SqlCodeTextBox";
+      this.SqlCodeTextBox.Size = new System.Drawing.Size(797, 400);
+      this.SqlCodeTextBox.TabIndex = 0;
+      // 
+      // ConvertButton
+      // 
+      this.ConvertButton.Location = new System.Drawing.Point(2, 3);
+      this.ConvertButton.Name = "ConvertButton";
+      this.ConvertButton.Size = new System.Drawing.Size(75, 29);
+      this.ConvertButton.TabIndex = 1;
+      this.ConvertButton.Text = "Convert";
+      this.ConvertButton.UseVisualStyleBackColor = true;
+      this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.AutoSize = true;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.ConvertButton);
+      this.Controls.Add(this.SqlCodeTextBox);
+      this.Name = "MainForm";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox SqlCodeTextBox;
+    private System.Windows.Forms.Button ConvertButton;
+  }
+}
+
---
+++ SqlKeywordConverter/MainForm.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SqlKeywordConverter
+{
+  public partial class MainForm : Form
+  {
+    public MainForm()
+    {
+      InitializeComponent();
+
+      // Enable drag and drop for the TextBox
+      SqlCodeTextBox.AllowDrop = true;
+
+      // Attach event handlers for drag and drop
+      SqlCodeTextBox.DragEnter += SqlCodeTextBox_DragEnter;
+      SqlCodeTextBox.DragDrop += SqlCodeTextBox_DragDrop;
+    }
+
+    private void ConvertButton_Click(object sender, EventArgs e)
+    {
+      string sqlCode = SqlCodeTextBox.Text;
+
+      // Exclude comments before converting SQL keywords to uppercase
+      sqlCode = ExcludeComments(sqlCode);
+
+      // Convert SQL keywords to uppercase using a regular expression
+      string convertedSqlCode = Regex.Replace(sqlCode, @"\b(SELECT|FROM|WHERE|AND|OR|INSERT|UPDATE|DELETE|CREATE|TABLE|INDEX|ALTER|DROP|JOIN)\b", match => match.Value.ToUpper(), RegexOptions.IgnoreCase);
+
+      // Update the textbox with the modified SQL code
+      SqlCodeTextBox.Text = convertedSqlCode;
+
+      // Copy the converted text to the clipboard
+      Clipboard.SetText(convertedSqlCode);
+
+      // Optionally, you can show a message to indicate that the text has been copied
+      MessageBox.Show("Converted text has been copied to the clipboard.", "Conversion Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+    }
+
+    private string ExcludeComments(string code)
+    {
+      // Exclude single-line comments (--)
+      int singleLineCommentIndex = code.IndexOf("--");
+      if (singleLineCommentIndex != -1)
+      {
+        code = code.Substring(0, singleLineCommentIndex);
+      }
+
+      // Exclude multiline comments (/* ... */)
+      int multiLineCommentStartIndex = code.IndexOf("/*");
+      int multiLineCommentEndIndex = code.IndexOf("*/");
+      if (multiLineCommentStartIndex != -1 && multiLineCommentEndIndex != -1 && multiLineCommentEndIndex > multiLineCommentStartIndex)
+      {
+        code = code.Remove(multiLineCommentStartIndex, multiLineCommentEndIndex - multiLineCommentStartIndex + 2);
+      }
+
+      return code;
+    }
+
+    private void SqlCodeTextBox_DragEnter(object sender, DragEventArgs e)
+    {
+      // Check if the data being dragged is a file
+      if (e.Data.GetDataPresent(DataFormats.FileDrop))
+      {
+        // Check if the effect should be set to Copy
+        if ((e.AllowedEffect & DragDropEffects.Copy) != 0)
+        {
+          e.Effect = DragDropEffects.Copy;
+        }
+        else
+        {
+          // If Copy is not allowed, set the effect to None
+          e.Effect = DragDropEffects.None;
+        }
+      }
+      else
+      {
+        e.Effect = DragDropEffects.None;
+      }
+    }
+
+
+    private void SqlCodeTextBox_DragDrop(object sender, DragEventArgs e)
+    {
+      // Get the file(s) dropped onto the TextBox
+      string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+      if (files.Length > 0)
+      {
+        // Read the content of the first file and set it as the TextBox text
+        string sqlCode = File.ReadAllText(files[0]);
+
+        // Update the TextBox
+        SqlCodeTextBox.Text = sqlCode;
+      }
+    }
+     
+  }
+
+}
